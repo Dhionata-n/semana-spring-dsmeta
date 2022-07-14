e5233f01fed80d0f7a83df6d3857df307758ceff
Dhionatan <dhionatan.dvg@gmail.com>
2022-07-13 21:50:14 -0300

    Api Test

---
+++ backend/src/main/java/com/dev/dsmeta/services/SaleService.java
@@ -0,0 +1,23 @@
+package com.dev.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dev.dsmeta.entities.Sale;
+import com.dev.dsmeta.repository.SaleRepository;
+
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findSales() {
+		return repository.findAll();
+		
+		
+	}
+}
